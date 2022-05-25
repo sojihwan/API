1ea6de896feaae7f6df06174ae94109a03226aa6
sojihwan <73238543+sojihwan@users.noreply.github.com>
2022-05-25 22:01:18 +0900

    5/25

---
+++ .history/contents/src/content/modal_20220525155458.js
@@ -0,0 +1,51 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/style-prop-object */
+import React from "react";
+import "../css/Modal.css"
+
+function Modal(props){
+  function Close(){
+    const modal = document.getElementById("modal");
+    const closeBtn = modal.querySelector(".close-area")
+    closeBtn.addEventListener("click", e => {
+    modal.style.display = "none"
+  })
+  }
+  function overlay_Close(){
+    const modal = document.getElementById("modal");
+    modal.addEventListener("click", e => {
+      const evTarget = e.target
+      if(evTarget.classList.contains("modal-overlay")) {
+          modal.style.display = "none"
+      }
+  })
+  }
+  let print 
+  print = props.id.similars
+  console.log(print)
+  return(
+    <div>
+      <div id="container">
+        <div id="lorem-ipsum"></div>
+      </div>
+      <div id="modal" className="modal-overlay" onClick={overlay_Close}>
+        <div className="modal-window">
+          <div className="title">
+            <h2>{props.id.titleStatement}</h2>
+          </div>
+          <div className="close-area" onClick={Close}>X</div>
+          
+          <div className="content">
+            <img id="ModalImg" src={props.id.thumbnailUrl !=null? props.id.thumbnailUrl:'img/null.jpg'}/>
+            <b id="author" className="ModalText">{props.id.author}</b>
+            <b id="publication" className="ModalText">{props.id.publication}</b>
+            <b id="similars" className="ModalText"></b>
+            <b>{props.id.id}</b>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Modal
---
+++ .history/contents/src/content/select_20220525164121.js
@@ -0,0 +1,86 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React,{useState,useEffect} from "react";
+import axios from "axios";
+import "../css/Select.css"
+import Modal from "../content/Modal.js";
+
+function Select(){
+  const [api, setApi] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [id,setId] = useState({})
+  const [page , setPage] = useState(1)
+  useEffect(() => {
+    const fetchapi = async () => {
+      try{
+        setApi(null);
+        setError(null);
+        setLoading(true);
+        const reponse = await axios.get(
+          `http://library.me.go.kr/pyxis-api/1/collections/1/search?all=k|a|library`
+        );
+        setApi(reponse.data)
+      }catch (e){
+         setError(e)
+      }
+      setLoading(false)
+    };
+    fetchapi();
+  },[])
+  
+  if (loading) return <div>로딩중..</div>
+  if(error) return <div>에러가 발생했습니다.</div>
+  if(!api) return null
+
+  function model(){
+
+    const modal = document.getElementById("modal");
+    modal.style.display = "flex";
+  }
+  const onBef = async () => {
+    if(page === 1 ){
+      alert("첫패이지 입니다");
+      return 0;
+    }
+    setPage(pag)
+    const reponse = await axios.get(
+      `http://library.me.go.kr/pyxis-api/1/collections/${page}/search?all=k|a|library`
+    );
+    setApi(reponse.data)
+  }
+  const onNaxt = async () => {
+    console.log(page)
+    page = page + 1
+    const reponse = await axios.get(
+      `http://library.me.go.kr/pyxis-api/1/collections/${page}/search?all=k|a|library`
+    );
+    setApi(reponse.data);
+  }
+  function idgive(key){
+    setId(key)
+  }
+  return (
+    <div>
+    {
+            api.data.list.map((item,index) => {
+              return(
+                <div className="img" key={index} onClick={() => {idgive(api.data.list[index])}}>
+                  <img className="bookimg" onClick={model} src={(api.data.list[index].thumbnailUrl != null)?api.data.list[index].thumbnailUrl:'img/ddd.jpg'}
+                  /><br/>
+                  <div className="Hidden">
+                  <b className="booktitle">{api.data.list[index].titleStatement}</b>
+                  </div>
+                </div>
+              );
+            }
+            )
+          }
+          <Modal id={id}/>
+        <button id="B" className="w-btn w-btn-green" onClick={onBef} type="button">이전</button>
+        <button id="N" className="w-btn w-btn-green" onClick={onNaxt} type="button">다음</button>
+        
+  </div>
+)
+}
+
+export default Select
